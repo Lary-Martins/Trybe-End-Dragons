cc6a17934f4edc9a621b2df81330a0838d737a03
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-03-26 17:10:15 -0300

    feat : cria classe Orc que estende de Race

---
+++ src/Races/Orc.ts
@@ -0,0 +1,17 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints = 74;
+  private static _instaces = 0;
+
+  static creatRacesInstances(): number {
+    this._instaces += 1;
+    return this._instaces;
+  }
+
+  get maxLifePoints():number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
