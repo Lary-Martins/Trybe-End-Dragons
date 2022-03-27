383c350aa8134df5c3bbb1915fb1c66fbd24bfc9
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-03-27 01:35:43 -0300

    feat : cria arquivo raiz de exportação

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,11 @@
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Ranger from './Ranger';
+
+export {
+  Mage,
+  Necromancer,
+  Warrior,
+  Ranger,
+};
