a32a316115c074337fe62ac40efa990e852b9fb2
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-03-27 01:35:12 -0300

    feat : cria classe Warrior que estende da classe Archetype

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,18 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private static _instances = 0;
+  private _energyType: EnergyType = 'stamina';
+
+  static createdArchetypeInstances(): number {
+    this._instances += 1;
+    return this._instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Warrior;
