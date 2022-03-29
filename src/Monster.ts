d0ccc8a19fe3ad6bb000c7e4b211b41fc42a4b3d
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-03-29 00:38:27 -0300

    feat: implementa métodos getter na classe

--- src/Monster.ts
+++ src/Monster.ts
@@ -8,3 +8,6 @@ class Mosnter implements SimpleFighter {
     this._lifePoints = 85;
     this._strength = 63;
   }
+
+  get lifePoints(): number { return this._lifePoints; }
+  get strength(): number { return this._strength; }
