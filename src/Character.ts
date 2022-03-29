90870112e83405f481d4a34029e6e4e9f70320d2
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-03-28 22:41:57 -0300

    feat: inicializa classe Character e seus atributos

---
+++ src/Character.ts
@@ -0,0 +1,29 @@
+import Race, { Elf } from './Races';
+import Archetype, { Mage } from './Archetypes';
+import Fighter from './Fighter';
+import Energy from './Energy';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy:Energy;
+
+  constructor(name:string) {
+    this._race = new Elf(name, 10);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._race.maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._dexterity = this._race.dexterity;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
